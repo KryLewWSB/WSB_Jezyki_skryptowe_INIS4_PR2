2f9d88ac1fb982850cc7853e1405b679fea97f8e
Krystian <krystian.lewandowski@teb.edu.pl>
2024-02-21 10:42:31 +0100

    6

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("\"Test\"")
-print('"Test"')
+print("Ania ma kota.", 5)
+#print("Ania ma kota." + 5)#bład: can only concatenate str (not "int") to str
+print("Ania ma kota." + str(5))
 
-print("Ania ma \nkota.")
+print("Ala", "ma", "kota", sep=" ", end=".")
+print("\nAnna" * 5)
+print("Anna\t" * 5)
+print("Anna " * 5)
