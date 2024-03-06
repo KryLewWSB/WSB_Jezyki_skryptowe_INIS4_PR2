6158f5f0136516c71cfeaf2deea15798a6282b1e
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-06 08:58:41 +0100

    13

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 print("Proszę o podanie liczby: ")
 pobranaLiczba = int(input())
 
-if pobranaLiczba % 2 == 0:
-    print(f"{pobranaLiczba} - ta liczba jest parzysta!")
-else:
-    print(f"{pobranaLiczba} - ta liczba  jest NIE parzysta!")
+try:
+    if pobranaLiczba % 2 == 0:
+        print(f"{pobranaLiczba} - ta liczba jest parzysta!")
+    else:
+        print(f"{pobranaLiczba} - ta liczba  jest NIE parzysta!")
+except:
+    print("Pojawił się błąd")
