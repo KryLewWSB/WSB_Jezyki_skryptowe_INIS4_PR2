9204327c1d687c8633b613374cbbb8d3ed45628f
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-06 09:06:34 +0100

    14

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-print("Proszę o podanie liczby: ")
-pobranaLiczba = int(input())
 
-try:
+
+while True:
+    print("Proszę o podanie liczby: ")
+    pobranaLiczba = int(input())
     if pobranaLiczba % 2 == 0:
         print(f"{pobranaLiczba} - ta liczba jest parzysta!")
     else:
         print(f"{pobranaLiczba} - ta liczba  jest NIE parzysta!")
-except:
-    print("Pojawił się błąd")
+    print("Jeśli zakończyć wpisz \"x\" i ENTER, jeśli liczyć dalej wpisz samo ENTER")
+    podanaWartosc = input()
+    if podanaWartosc == "x":
+        break
