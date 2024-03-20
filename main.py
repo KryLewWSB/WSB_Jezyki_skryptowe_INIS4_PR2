25594edc04a5d6d9e37f60595a19d088ca14787b
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-20 10:18:45 +0100

    20_03_24___6_klasa_uczen

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+class Uczen:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.listaOcen = []
+        self.srednia = 0
+    def dodajOcene(self, ocena):
+        self.listaOcen.append(ocena)
+    def wypiszDAneUcznia(self):
+        print(f"Imię ucznia: {self.imie} | Nazwisko ucznia: {self.nazwisko} | "
+              f"Oceny ucznia: {self.listaOcen}")
+    def wyliczSredniaOcenUcznia(self):
+        sumaOcen = 0
+        #Robimy sumowanie w pętli wszystkich ocen:
+        for i in self.listaOcen:
+            sumaOcen += i
+        #Sumę ocen dzielimy przez ilosc ocen
+        srednia = sumaOcen / len(self.listaOcen)
+        print(f"Średnia ocen ucznia {self.imie} {self.nazwisko} to: {srednia}")
 
-zbior = set()
-#Lotto (wykorzystanie set - zbioru slajd 47/48 z prezentacji modul 3)
-while len(zbior) < 6:
-    print("Podaj liczbę: ")
-    liczba = int(input())
-    zbior.add(liczba)
+uczen = Uczen("Adam", "Kowal")
+uczen.dodajOcene(5)
+uczen.dodajOcene(5)
+uczen.dodajOcene(4)
+uczen.dodajOcene(3)
 
-print(zbior)
-
-
+uczen.wyliczSredniaOcenUcznia()
