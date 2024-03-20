892405a903a8a7216a2145084b8fe15f3154d030
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-20 09:31:49 +0100

    20_03_24___3_lottopodpowiedz

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 zbior = set()
 #Lotto (wykorzystanie set - zbioru slajd 47/48 z prezentacji modul 3)
-while len(zbior) <= 6:
+while len(zbior) < 6:
     print("Podaj liczbę: ")
     liczba = int(input())
     zbior.add(liczba)
