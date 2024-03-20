b759e42d45ffc26272e41335a401e350f1122e2e
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-20 09:30:42 +0100

    20_03_24___2_lottopodpowiedz

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-def wyliczPierwszyProgPodatkowy():
-    #Wyliczenie dla 1 progu podatkowego:
-    podatekPierwszyProg = 0
-    if dochod <= 120_000:
-        podatekPierwszyProg = dochod * 0.12
-        print(f"Od dochodu: {dochod}\n Musisz zapłacić tyle podatku: {podatekPierwszyProg}")
 
+zbior = set()
+#Lotto (wykorzystanie set - zbioru slajd 47/48 z prezentacji modul 3)
+while len(zbior) <= 6:
+    print("Podaj liczbę: ")
+    liczba = int(input())
+    zbior.add(liczba)
 
-def wyliczDrugiProgPodatkowy():
-    if dochod >= 120_000:
-        pierwszyProg = 120_000 * 0.12
-        drugiProg = (dochod - 120_000) * 0.32
-        print(f"Twój dochód to: {dochod}\nMusisz zapłacić podatek w "
-              f"wysokości: {pierwszyProg + drugiProg}\nPodatek naliczony w I progu: {pierwszyProg}"
-              f"\nPodatek naliczony w II progu podatkowym: {drugiProg}")
-
-print("Proszę o podanie dochodu rocznego: ")
-dochod = int(input())
-dochod = dochod - 30_000  #Pomniejszenie o kwotę wolną od podatku
-wyliczPierwszyProgPodatkowy()
-wyliczDrugiProgPodatkowy()
-
-
-
-
+print(zbior)
 
