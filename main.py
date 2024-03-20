21df1946cd8e00f7e18e93d23cf4c7fcf4418b6e
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-20 08:46:47 +0100

    20_03_24___1

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+def wyliczPierwszyProgPodatkowy():
+    #Wyliczenie dla 1 progu podatkowego:
+    podatekPierwszyProg = 0
+    if dochod <= 120_000:
+        podatekPierwszyProg = dochod * 0.12
+        print(f"Od dochodu: {dochod}\n Musisz zapłacić tyle podatku: {podatekPierwszyProg}")
 
 
-while True:
-    print("Proszę o podanie liczby: ")
-    pobranaLiczba = int(input())
-    if pobranaLiczba % 2 == 0:
-        print(f"{pobranaLiczba} - ta liczba jest parzysta!")
-    else:
-        print(f"{pobranaLiczba} - ta liczba  jest NIE parzysta!")
-    print("Jeśli zakończyć wpisz \"x\" i ENTER, jeśli liczyć dalej wpisz samo ENTER")
-    podanaWartosc = input()
-    if podanaWartosc == "x":
-        break
+def wyliczDrugiProgPodatkowy():
+    if dochod >= 120_000:
+        pierwszyProg = 120_000 * 0.12
+        drugiProg = (dochod - 120_000) * 0.32
+        print(f"Twój dochód to: {dochod}\nMusisz zapłacić podatek w "
+              f"wysokości: {pierwszyProg + drugiProg}\nPodatek naliczony w I progu: {pierwszyProg}"
+              f"\nPodatek naliczony w II progu podatkowym: {drugiProg}")
+
+print("Proszę o podanie dochodu rocznego: ")
+dochod = int(input())
+dochod = dochod - 30_000  #Pomniejszenie o kwotę wolną od podatku
+wyliczPierwszyProgPodatkowy()
+wyliczDrugiProgPodatkowy()
+
+
+
+
+
